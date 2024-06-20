87a97a1735fa95ffc3096f6e9848b53612672fcb
Phil Prasek <prasek@gmail.com>
2024-06-19 21:34:19 -0700

    replace shipment child workflow call with a Nexus call
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 docker volume rm deployments_api-data
 
 ./bin/temporal operator namespace create --namespace default
+
 ./bin/temporal operator nexus endpoint delete --name billing
 ./bin/temporal operator nexus endpoint create --name billing --target-namespace default --target-task-queue billing --description test123
+
+./bin/temporal operator nexus endpoint delete --name shipment
+./bin/temporal operator nexus endpoint create --name shipment --target-namespace default --target-task-queue shipments --description test123
