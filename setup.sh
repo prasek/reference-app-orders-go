bab04caa0c82de4b1c4dbf778f14e7966d3fc8cf
Phil Prasek <prasek@gmail.com>
2024-06-20 12:09:11 -0700

    minor cleanup
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+( cd deployments; docker compose down)
+
 docker volume rm deployments_api-data
 
 ./bin/temporal operator namespace create --namespace default
@@ -10,3 +12,5 @@ docker volume rm deployments_api-data
 
 ./bin/temporal operator nexus endpoint delete --name order
 ./bin/temporal operator nexus endpoint create --name order --target-namespace default --target-task-queue orders --description test123
+
+(cd deployments; docker compose up --build)
