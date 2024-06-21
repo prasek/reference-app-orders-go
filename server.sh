add430b004d8d94d8ac7f44d5c98aff94788abd9
Phil Prasek <prasek@gmail.com>
2024-06-20 22:10:18 -0700

    build temporal CLI from source with Nexus support via submodule
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/temporal server start-dev --dynamic-config-value system.enableNexus=true --http-port 7243
+./temporal server start-dev --dynamic-config-value system.enableNexus=true --http-port 7243
