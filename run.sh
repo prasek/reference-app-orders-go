463e0fdd61f03f7a1c7ebd5dea48a12f7b0309d6
Phil Prasek <prasek@gmail.com>
2024-07-12 13:06:14 -0700

    wip
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- run.sh
+++ run.sh
@@ -4,16 +4,24 @@ set -x
 ( cd deployments; docker compose down)
 
 docker volume rm deployments_api-data
+docker volume rm deployments_namespace-certs
 
-./temporal operator namespace create --namespace default
+tcld --server "saas-api.tmprl-test.cloud:443" nexus endpoint create \
+  --name billing \
+  --target-task-queue billing \
+  --target-namespace nexus-demo-bugbash.temporal-dev \
+  --allow-namespace nexus-demo-bugbash.temporal-dev \
+  --description-file description.md
 
-./temporal operator nexus endpoint delete --name billing
-./temporal operator nexus endpoint create --name billing --target-namespace default --target-task-queue billing --description test123
-
-./temporal operator nexus endpoint delete --name shipment
-./temporal operator nexus endpoint create --name shipment --target-namespace default --target-task-queue shipments --description test123
-
-./temporal operator nexus endpoint delete --name order
-./temporal operator nexus endpoint create --name order --target-namespace default --target-task-queue orders --description test123
+#./temporal operator namespace create --namespace default
+#
+#./temporal operator nexus endpoint delete --name billing
+#./temporal operator nexus endpoint create --name billing --target-namespace default --target-task-queue billing --description test123
+#
+#./temporal operator nexus endpoint delete --name shipment
+#./temporal operator nexus endpoint create --name shipment --target-namespace default --target-task-queue shipments --description test123
+#
+#./temporal operator nexus endpoint delete --name order
+#./temporal operator nexus endpoint create --name order --target-namespace default --target-task-queue orders --description test123
 
 (cd deployments; docker compose up --build)
