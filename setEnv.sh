bbddecdb31c03e2ec177867c6a818021559ee227
Phil Prasek <prasek@gmail.com>
2024-07-17 17:07:09 -0700

    cleanup setEnv.sh
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- setEnv.sh
+++ setEnv.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# for run.sh and temporal.sh
-if [[ $TEMPORAL_ENV = "billing" ]]; then
-    export TEMPORAL_ADDRESS="nexus-demo-billing.a2dd6.tmprl.cloud:7233"
-    export TEMPORAL_NAMESPACE="nexus-demo-billing.a2dd6"
-else
-    export TEMPORAL_ADDRESS="nexus-demo-monolith.a2dd6.tmprl.cloud:7233"
-    export TEMPORAL_NAMESPACE="nexus-demo-monolith.a2dd6"
-fi
+export TEMPORAL_NAMESPACE_MONOLITH="my-monolith-namespace.account"
+export TEMPORAL_NAMESPACE_BILLING="my-billing-namespace.account"
 
+# endpoints are global to your account like namespaces
+export NEXUS_ENDPOINT_ORDER="my-order-endpoint"
+export NEXUS_ENDPOINT_BILLING="my-billing-endpoint"
+export NEXUS_ENDPOINT_SHIPMENT="my-shipment-endpoint"
+
+# this likely doesn't need to chage for your environment
 export TEMPORAL_TLS_CERT="$HOME/nexus-demo/certs/ca.pem"
 export TEMPORAL_TLS_KEY="$HOME/nexus-demo/certs/ca.key"
-
-# for init.sh
 export TEMPORAL_OPS_API="saas-api.tmprl.cloud:443"
-export TEMPORAL_NAMESPACE_BILLING="nexus-demo-billing.a2dd6"
+
+# for run.sh and temporal.sh
+if [[ $TEMPORAL_ENV = "billing" ]]; then
+    export TEMPORAL_ADDRESS="${TEMPORAL_NAMESPACE_BILLING}.tmprl.cloud:7233"
+    export TEMPORAL_NAMESPACE="${TEMPORAL_NAMESPACE_BILLING}"
+else
+    export TEMPORAL_ADDRESS="${TEMPORAL_NAMESPACE_MONOLITH}.tmprl.cloud:7233"
+    export TEMPORAL_NAMESPACE="${TEMPORAL_NAMESPACE_MONOLITH}"
+fi
