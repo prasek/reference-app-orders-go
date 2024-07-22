0caa4f16136f9363f725b9a6466ad765c41e6ffb
Phil Prasek <prasek@gmail.com>
2024-07-22 12:30:43 -0700

    cleanup
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- run.sh
+++ run.sh
@@ -5,15 +5,15 @@ set -x
 
 docker volume rm deployments_api-data
 
-./temporal operator namespace create --namespace default
+./bin/temporal operator namespace create --namespace default
 
-./temporal operator nexus endpoint delete --name billing
-./temporal operator nexus endpoint create --name billing --target-namespace default --target-task-queue billing --description test123
+./bin/temporal operator nexus endpoint delete --name billing
+./bin/temporal operator nexus endpoint create --name billing --target-namespace default --target-task-queue billing --description test123
 
-./temporal operator nexus endpoint delete --name shipment
-./temporal operator nexus endpoint create --name shipment --target-namespace default --target-task-queue shipments --description test123
+./bin/temporal operator nexus endpoint delete --name shipment
+./bin/temporal operator nexus endpoint create --name shipment --target-namespace default --target-task-queue shipments --description test123
 
-./temporal operator nexus endpoint delete --name order
-./temporal operator nexus endpoint create --name order --target-namespace default --target-task-queue orders --description test123
+./bin/temporal operator nexus endpoint delete --name order
+./bin/temporal operator nexus endpoint create --name order --target-namespace default --target-task-queue orders --description test123
 
 (cd deployments; docker compose up --build)
