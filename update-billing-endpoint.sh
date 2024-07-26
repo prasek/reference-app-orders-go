c4b41c3444ec2e92ba9e0996aee314a8fe795a83
Phil Prasek <prasek@gmail.com>
2024-07-26 14:01:45 -0700

    cleanup
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- update-billing-endpoint.sh
+++ update-billing-endpoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 
 ./bin/temporal operator nexus endpoint delete --name billing
 ./bin/temporal operator nexus endpoint create --name billing --target-namespace billing --target-task-queue billing --description test123
